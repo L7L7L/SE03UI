043b783ef353c34eae7b2aab4f360efb4eb3289b
yaoyaoshe <1974682919@qq.com>
2024-05-29 17:19:15 +0800

    1

--- build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui_mainwindow.h
+++ build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/ui_mainwindow.h
@@ -28,14 +28,14 @@ class Ui_MainWindow
 public:
     QWidget *centralwidget;
     QTabWidget *tabWidget;
-    QWidget *tab;
+    QWidget *login;
     QLabel *label_2;
-    QLineEdit *lineEdit;
     QPushButton *pushButton;
     QLineEdit *lineEdit_2;
     QCheckBox *checkBox;
     QLabel *label;
     QLabel *label_3;
+    QLineEdit *lineEdit;
     QWidget *tab_2;
     QMenuBar *menubar;
     QStatusBar *statusbar;
@@ -51,40 +51,46 @@ public:
         tabWidget = new QTabWidget(centralwidget);
         tabWidget->setObjectName("tabWidget");
         tabWidget->setGeometry(QRect(0, 0, 370, 455));
-        tab = new QWidget();
-        tab->setObjectName("tab");
-        label_2 = new QLabel(tab);
+        tabWidget->setTabShape(QTabWidget::Rounded);
+        tabWidget->setElideMode(Qt::ElideNone);
+        tabWidget->setUsesScrollButtons(false);
+        tabWidget->setTabsClosable(false);
+        tabWidget->setTabBarAutoHide(false);
+        login = new QWidget();
+        login->setObjectName("login");
+        label_2 = new QLabel(login);
         label_2->setObjectName("label_2");
         label_2->setGeometry(QRect(20, 180, 37, 20));
-        lineEdit = new QLineEdit(tab);
-        lineEdit->setObjectName("lineEdit");
-        lineEdit->setGeometry(QRect(20, 120, 320, 40));
-        lineEdit->setStyleSheet(QString::fromUtf8("background-color:rgb(255, 255, 255)"));
-        pushButton = new QPushButton(tab);
+        pushButton = new QPushButton(login);
         pushButton->setObjectName("pushButton");
         pushButton->setGeometry(QRect(20, 310, 320, 36));
         pushButton->setStyleSheet(QString::fromUtf8("background-color : rgb(168, 85, 247);\n"
 "color : white;\n"
 "\n"
 ""));
-        lineEdit_2 = new QLineEdit(tab);
+        lineEdit_2 = new QLineEdit(login);
         lineEdit_2->setObjectName("lineEdit_2");
         lineEdit_2->setGeometry(QRect(20, 220, 320, 40));
         lineEdit_2->setStyleSheet(QString::fromUtf8("background-color:rgb(255, 255, 255)"));
-        checkBox = new QCheckBox(tab);
+        checkBox = new QCheckBox(login);
         checkBox->setObjectName("checkBox");
         checkBox->setGeometry(QRect(20, 280, 81, 18));
         checkBox->setStyleSheet(QString::fromUtf8(""));
-        label = new QLabel(tab);
+        label = new QLabel(login);
         label->setObjectName("label");
         label->setGeometry(QRect(20, 80, 37, 20));
-        label_3 = new QLabel(tab);
+        label_3 = new QLabel(login);
         label_3->setObjectName("label_3");
         label_3->setGeometry(QRect(145, 20, 82, 75));
-        label_3->setPixmap(QPixmap(QString::fromUtf8(":/new/prefix1/logo.png")));
+        label_3->setPixmap(QPixmap(QString::fromUtf8(":/new/prefix1/image/logo.png")));
         label_3->setScaledContents(true);
         label_3->setWordWrap(false);
-        tabWidget->addTab(tab, QString());
+        lineEdit = new QLineEdit(login);
+        lineEdit->setObjectName("lineEdit");
+        lineEdit->setGeometry(QRect(20, 120, 319, 40));
+        lineEdit->setMinimumSize(QSize(250, 40));
+        lineEdit->setStyleSheet(QString::fromUtf8("background-color:rgb(255, 255, 255)"));
+        tabWidget->addTab(login, QString());
         tab_2 = new QWidget();
         tab_2->setObjectName("tab_2");
         tabWidget->addTab(tab_2, QString());
@@ -99,6 +105,9 @@ public:
 
         retranslateUi(MainWindow);
 
+        tabWidget->setCurrentIndex(0);
+
+
         QMetaObject::connectSlotsByName(MainWindow);
     } // setupUi
 
@@ -106,14 +115,14 @@ public:
     {
         MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "\345\255\246\346\234\257\345\260\217\345\212\251\346\211\213-\347\231\273\345\275\225", nullptr));
         label_2->setText(QCoreApplication::translate("MainWindow", "\345\257\206\347\240\201", nullptr));
-        lineEdit->setText(QString());
-        lineEdit->setPlaceholderText(QCoreApplication::translate("MainWindow", "\350\257\267\350\276\223\345\205\245\351\202\256\347\256\261", nullptr));
         pushButton->setText(QCoreApplication::translate("MainWindow", "\347\231\273\345\275\225", nullptr));
         lineEdit_2->setText(QString());
         lineEdit_2->setPlaceholderText(QCoreApplication::translate("MainWindow", "\350\257\267\350\276\223\345\205\245\345\257\206\347\240\201", nullptr));
         checkBox->setText(QCoreApplication::translate("MainWindow", "\350\256\260\344\275\217\345\257\206\347\240\201", nullptr));
         label->setText(QCoreApplication::translate("MainWindow", "\351\202\256\347\256\261", nullptr));
-        tabWidget->setTabText(tabWidget->indexOf(tab), QCoreApplication::translate("MainWindow", "Tab 1", nullptr));
+        lineEdit->setText(QString());
+        lineEdit->setPlaceholderText(QCoreApplication::translate("MainWindow", "\350\257\267\350\276\223\345\205\245\351\202\256\347\256\261", nullptr));
+        tabWidget->setTabText(tabWidget->indexOf(login), QCoreApplication::translate("MainWindow", "Tab 1", nullptr));
         tabWidget->setTabText(tabWidget->indexOf(tab_2), QCoreApplication::translate("MainWindow", "Tab 2", nullptr));
     } // retranslateUi
 
